4c8bb40794c5f867a81fedc979ff4fba6164237b
Massachusetts Institute of Mathematics <159593968+NewYorkGeneralGroup@users.noreply.github.com>
2024-08-10 04:43:38 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+```python
+from setuptools import setup, find_packages
+
+setup(
+    name="arpvnet",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.7.0",
+        "numpy>=1.19.2",
+    ],
+)
+```
